9c0790f812accc8bcb49320dc59056532fea2177
Hristiyan Dodov <h.dodov@gmail.com>
2019-08-20 12:13:58 +0300

    Implement plural operands correctly

--- tests/suites/LanguageTest.php
+++ tests/suites/LanguageTest.php
@@ -5,26 +5,23 @@ namespace Oblik\Pluralization;
 use ReflectionMethod;
 use PHPUnit\Framework\TestCase;
 
+
 final class LanguageTest extends TestCase
 {
+    /**
+     * @see http://unicode.org/reports/tr35/tr35-numbers.html#Plural_Operand_Examples
+     */
     public function testNumberParsing()
     {
         $method = new ReflectionMethod(Language::class, 'parseNumber');
         $method->setAccessible(true);
 
-        $this->assertEquals(['integer' => 0, 'fraction' => null], $method->invoke(null, 0));
-        $this->assertEquals(['integer' => 0, 'fraction' => null], $method->invoke(null, 0.0));
-        $this->assertEquals(['integer' => 0, 'fraction' => 0], $method->invoke(null, '0.0'));
-        $this->assertEquals(['integer' => 0, 'fraction' => 0.1], $method->invoke(null, 0.1));
-        $this->assertEquals(['integer' => 0, 'fraction' => 0.1], $method->invoke(null, '0.1'));
-
-        $this->assertEquals(['integer' => 1, 'fraction' => null], $method->invoke(null, 1));
-        $this->assertEquals(['integer' => 1, 'fraction' => null], $method->invoke(null, 1.0));
-        $this->assertEquals(['integer' => 1, 'fraction' => 0], $method->invoke(null, '1.0'));
-        $this->assertEquals(['integer' => 1, 'fraction' => 0.004], $method->invoke(null, 1.004));
-        
-        $this->assertEquals(['integer' => 0, 'fraction' => null], $method->invoke(null, -0.0));
-        $this->assertEquals(['integer' => -1, 'fraction' => 0.5], $method->invoke(null, -1.5));
-        $this->assertEquals(['integer' => -1, 'fraction' => 0.5], $method->invoke(null, '-1.5'));
+        $this->assertEquals([1, 1, 0], $method->invoke(null, 1));
+        $this->assertEquals(['1.0', 1, 1], $method->invoke(null, '1.0'));
+        $this->assertEquals(['1.00', 1, 2], $method->invoke(null, '1.00'));
+        $this->assertEquals([1.3, 1, 1], $method->invoke(null, 1.3));
+        $this->assertEquals(['1.30', 1, 2], $method->invoke(null, '1.30'));
+        $this->assertEquals([1.03, 1, 2], $method->invoke(null, 1.03));
+        $this->assertEquals(['1.230', 1, 3], $method->invoke(null, '1.230'));
     }
 }

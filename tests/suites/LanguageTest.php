4ce26763b7ca6af61fdaf85234017c9563a0f739
Neil Daniels <neil.here@gmail.com>
2020-03-29 15:15:48 -0700

    Added w,f,t variable support

--- tests/suites/LanguageTest.php
+++ tests/suites/LanguageTest.php
@@ -16,12 +16,12 @@ final class LanguageTest extends TestCase
         $method = new ReflectionMethod(Language::class, 'parseNumber');
         $method->setAccessible(true);
 
-        $this->assertEquals([1, 1, 0], $method->invoke(null, 1));
-        $this->assertEquals(['1.0', 1, 1], $method->invoke(null, '1.0'));
-        $this->assertEquals(['1.00', 1, 2], $method->invoke(null, '1.00'));
-        $this->assertEquals([1.3, 1, 1], $method->invoke(null, 1.3));
-        $this->assertEquals(['1.30', 1, 2], $method->invoke(null, '1.30'));
-        $this->assertEquals([1.03, 1, 2], $method->invoke(null, 1.03));
-        $this->assertEquals(['1.230', 1, 3], $method->invoke(null, '1.230'));
+        $this->assertEquals([1, 1, 0, 0, 0, 0], $method->invoke(null, 1));
+        $this->assertEquals(['1.0', 1, 1, 0, 0, 0], $method->invoke(null, '1.0'));
+        $this->assertEquals(['1.00', 1, 2, 0, 0, 0], $method->invoke(null, '1.00'));
+        $this->assertEquals([1.3, 1, 1, 1, 3, 3], $method->invoke(null, 1.3));
+        $this->assertEquals(['1.30', 1, 2, 1, 30, 3], $method->invoke(null, '1.30'));
+        $this->assertEquals([1.03, 1, 2, 2, 3, 3], $method->invoke(null, 1.03));
+        $this->assertEquals(['1.230', 1, 3, 2, 230, 23], $method->invoke(null, '1.230'));
     }
 }

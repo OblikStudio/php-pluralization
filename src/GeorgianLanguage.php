b73755e161e3f5878495a3dd2ac1fc38c26bcaf7
Neil Daniels <neil.here@gmail.com>
2020-03-29 17:00:53 -0700

    Added Georgian and Lithuanian

---
+++ src/GeorgianLanguage.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Oblik\Pluralization;
+
+class GeorgianLanguage extends Language
+{
+    static function cardinal(float $n, int $i, int $v)
+    {
+        if ($n == 1) {
+            return ONE;
+        }
+        return OTHER;
+    }
+
+    static function ordinal(int $n)
+    {
+        $mod100 = $n % 100;
+        
+        if ($n == 1) {
+            return ONE;
+        }
+        elseif ($n == 0 || (($mod100 >= 2 && $mod100 <= 20) || $mod100 === 40 || $mod100 == 60 || $mod100 == 80)) {
+            return MANY;
+        }
+        return OTHER;
+    }
+
+    const RANGE = [
+        ONE . OTHER => ONE,
+        OTHER . ONE => OTHER,
+        OTHER . OTHER => OTHER,
+    ];
+}
---
+++ src/LithuanianLanguage.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Oblik\Pluralization;
+
+class LithuanianLanguage extends Language
+{
+    static function cardinal(float $n, int $i, int $v, int $w, int $f, int $t)
+    {
+        $mod10 = $n % 10;
+        $mod100 = $n % 100;
+        $hasNonZeroDecimal = $n != $i;
+        
+        if (!$hasNonZeroDecimal && $mod10 == 1 && ($mod100 < 11 || $mod100 > 19)) {
+            return ONE;
+        }
+        elseif (!$hasNonZeroDecimal && ($mod10 >= 2 && $mod10 < 19) && ($mod100 < 11 || $mod100 > 19)) {
+            return FEW;
+        }
+        elseif ($f != 0) {
+            return MANY;
+        }
+        return OTHER;
+    }
+
+    static function ordinal(int $n)
+    {
+        return OTHER;
+    }
+
+    const RANGE = [
+        ONE . ONE => ONE,
+        ONE . FEW => FEW,
+        ONE . MANY => MANY,
+        ONE . OTHER => OTHER,
+        FEW . ONE => ONE,
+        FEW . FEW => FEW,
+        FEW . MANY => MANY,
+        FEW . OTHER => OTHER,
+        MANY . ONE => ONE,
+        MANY . FEW => FEW,
+        MANY . MANY => MANY,
+        MANY . OTHER => OTHER,
+        OTHER . ONE => ONE,
+        OTHER . FEW => FEW,
+        OTHER . MANY => MANY,
+        OTHER . OTHER => OTHER,
+    ];
+}
